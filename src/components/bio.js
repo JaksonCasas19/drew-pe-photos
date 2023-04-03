2dfdcddf11b1848d51b91380115d29b2a314c230
Jakson Casas <jaksoncasas@gmail.com>
2023-04-03 17:41:55 -0500

    drop text

--- src/components/bio.js
+++ src/components/bio.js
@@ -33,24 +33,26 @@ const Bio = () => {
   return (
     <div className="bio">
       <div className="flex-row">
-      <StaticImage
-        className="bio-avatar"
-        layout="fixed"
-        formats={["auto", "webp", "avif"]}
-        src="https://storage.googleapis.com/support-forums-api/avatar/profile-82939246-10154479357538749704.jpg"
-        width={50}
-        height={50}
-        quality={95}
-        alt="Profile picture"
-      /></div>
+        <StaticImage
+          className="bio-avatar"
+          layout="fixed"
+          formats={["auto", "webp", "avif"]}
+          src="https://storage.googleapis.com/support-forums-api/avatar/profile-82939246-10154479357538749704.jpg"
+          width={50}
+          height={50}
+          quality={95}
+          alt="Profile picture"
+        />
+      </div>
       <div className="flex-column">
         <p className="greeting">¡Bienvenido👋!</p>
-      {author?.name && (
-        <p className="about-avatar">
-          En esta web vas a encontrar apuntes sobre Google Fotos. Creado por {author.name}, {author?.summary || null}
-          {` `}
-        </p>
-      )}</div>
+        {author?.name && (
+          <p className="about-avatar">
+            En esta web vas a encontrar apuntes sobre Google Fotos.
+            {` `}
+          </p>
+        )}
+      </div>
     </div>
   )
 }

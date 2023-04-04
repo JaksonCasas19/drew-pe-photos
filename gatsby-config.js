3417e883a5b9f819f41e6662fecdf18a124de200
Jakson Casas <jaksoncasas@gmail.com>
2023-04-03 20:01:55 -0500

    analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,9 +15,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // The property ID; the tracking code won't be generated without it
         trackingId: "G-W7Q4HH9CZD",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: true,
       },
     },
